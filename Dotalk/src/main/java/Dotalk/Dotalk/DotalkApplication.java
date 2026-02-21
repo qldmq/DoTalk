92eb59e9b8a930ee2c4d77c7014ce62b716153ff
qldmq <ksh13345@naver.com>
2026-02-22 02:40:59 +0900

    init project

---
+++ Dotalk/src/main/java/Dotalk/Dotalk/DotalkApplication.java
@@ -0,0 +1,13 @@
+package Dotalk.Dotalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DotalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DotalkApplication.class, args);
+	}
+
+}
---
+++ Dotalk/src/test/java/Dotalk/Dotalk/DotalkApplicationTests.java
@@ -0,0 +1,13 @@
+package Dotalk.Dotalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DotalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
