7f704cd517e568dde735eceb950a33518e8a7009
qldmq <ksh13345@naver.com>
2026-03-14 23:31:24 +0900

    feat | User 엔티이 추가

--- Dotalk/src/main/java/Dotalk/Dotalk/domain/entity/User.java
+++ Dotalk/src/main/java/Dotalk/Dotalk/domain/entity/User.java
@@ -1,4 +1,34 @@
 package Dotalk.Dotalk.domain.entity;
 
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String role;
+
+    @Builder
+    public User(String email, String name, String role) {
+        this.email = email;
+        this.name = name;
+        this.role = role;
+    }
 }
